0c5a6c174f4d84defa599a49c0bb8b95036c0eba
Akashkumar-Jeyaramans <jeyakashkumar@gmail.com>
2021-09-23 21:00:05 +0530

    full app

--- Button.js
+++ Button.js
@@ -4,7 +4,9 @@ import Input from "./Input1";
 class Button extends Component{
     constructor(props){
         super(props);
-         
+        this.state={
+            weight:0,height:0,bmi:0
+            } 
     }
 
  
@@ -12,18 +14,52 @@ class Button extends Component{
     render(){
         return(
             <>
+         <div className="container">
             <div className="row">
                 <div className="col-3">
-            <label>Height</label><Input ></Input><p>feet</p>
+                     <label>Height</label>
+                </div>
             </div>
-            <div className="col-3">
-            <Input ></Input><p>inches</p><br></br>
-            </div>
-            </div>
-            <div className="row">
-            <label>Weight</label>
-            <Input ></Input>
-            </div>
+
+                <div className="row">
+                    <div className="col-2">
+                         <Input ></Input>
+                    </div>
+
+                    <div className="col-3">
+                        <label>Feet</label>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-2">
+                        <Input ></Input>
+                    </div>
+
+                    <div className="col-3">
+                        <p>inches</p><br></br>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-3">
+                        <label>Weight</label>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-2">
+                        <Input ></Input>
+                    </div>
+
+                    <div className="col-3">
+                        <p>Kilograms</p><br></br>
+                    </div>
+                </div>
+                
+                <div>
+                    <button>submit</button>
+                </div>
+                 
+            
+        </div>
             </>
         );
     }
--- Input1.js
+++ Input1.js
@@ -9,7 +9,6 @@ class Input extends Component{
         return(
             <>
              <input></input>
-            
             </>
         );
     }
